da5369795b3e9a0f987cfcd4a7ee241377234d8a
Lian0123 <lienyungli123@gmail.com>
2018-06-30 19:52:20 +0800

    修正執行的錯誤與精簡化功能

--- FMiCa-TW.sh
+++ FMiCa-TW.sh
@@ -6,11 +6,12 @@
 #	2017/05/25 Lian0123 version:0.0.1
 #	2017/07/08 Lian0123 version:0.0.2
 #	2017/07/10 Lian0123 version:0.0.3
-#       2017/07/29 Lian0123 version:0.0.4
-#       2017/07/29 Lian0123 version:0.0.41
-#       2017/08/04 Lian0123 version:0.0.42
-#       2017/08/04 Lian0123 version:0.0.42-1
-#       2018/04/29 Lian0123 version:0.0.43
+# 2017/07/29 Lian0123 version:0.0.4
+# 2017/07/29 Lian0123 version:0.0.41
+# 2017/08/04 Lian0123 version:0.0.42
+# 2017/08/04 Lian0123 version:0.0.42-1
+# 2018/04/29 Lian0123 version:0.0.43
+# 2018/04/30 Lian0123 version:0.0.43-1
 ##提升權限
 
 echo -e "\033[1;32m"
@@ -34,13 +35,8 @@ echo -e "  ################################################################  "
 echo -e "                                                                    "
 echo -e "\033[0m"
 
-while [ 1 ]
-  do
-    printf "你同意要使用此腳本安裝程式嗎？現在^C還來得及(任意鍵繼續)"
-    read select
-    break
-  done
-
+printf "你同意要使用此腳本安裝程式嗎？現在^C中斷還來得及(任意鍵繼續)"
+read select
 
 ##UPDATE
 echo -e "\033[1;32m"
@@ -83,83 +79,67 @@ apt install -y make
 
 ##字體安裝
 
-##while [ 1 ]
+##while :
   ##do
     ##wget https://noto-website.storage.googleapis.com/pkgs/Noto-hinted.zip
     ##icon下載與安裝
   ##done
 
-while [ 1 ]
+while :
   do
     echo -e "\033[1;32m"
     echo "你需要安裝其他的美觀圖標嗎？(請輸入選項數字)"
-    echo "1.Google-paper-icon"
-    echo "2.Numix-icon-circle"
-    echo "3.Breeze-icon"
-    echo "4.No,thank..."
+    echo "1) Google-paper-icon"
+    echo "2) Numix-icon-circle"
+    echo "3) Breeze-icon"
+    echo "4) 結束"
     printf "請輸入你的選項："
     read select
     echo -e "\033[0m"
-    if [ "$select" -le "4" ] || [ "$select" -ge "1"]
-      then break
-    fi
-  done
-
-if [ "$select" == "1" ] 
-  then
-  ##來源：paper icon 的 github(修改)
-  echo -e "\033[1;32m"
-  echo "正在安裝Google-paper-icon..."
-  echo -e "\033[0m"
-  mkdir .PAPER-ICON
-  cd .PAPER-ICON
-  git clone https://github.com/snwh/paper-icon-theme.git
-  ./autogen.sh
-  make
-  make install
-  cd ..
-  rm -rf .PAPER-ICON
-elif [ "$echo" == "2" ] 
-  then
-  ##來源：numix-icon 的 github（修改）
-  echo -e "\033[1;32m"
-  echo "正在安裝Numix-icon-circle..."
-  echo -e "\033[0m"
-  add-apt-repository ppa：numix / ppa
-  apt update
-  apt install numix-icon-theme-circle
-  apt install 
-elif [ "$echo" == "3" ]
-  then
-  echo -e "\033[1;32m"
-  echo "正在安裝Breeze-icon..."
-  echo -e "\033[0m"
-  mkdir .BREEZE-ICON
-  cd .BREEZE-ICON
-  git clone https://github.com/NitruxSA/breeze-icon-theme.git
-  cp Breeze /usr/share/icons
-  cp Breeze\ Dark /usr/share/icons
-  cd ..
-  rm -rf .BREEZE-ICON
-elif [ "$echo" == "4" ]
-  then echo "好的，本次不安裝圖標"
-fi
-
-##工作列透明化選擇
-while [ 1 ]
-  do
-    echo -e "\033[1;32m"
-    printf "你想要安裝透明工作列嗎？(Y/N)"
-    read IsTable
-    echo -e "\033[0m"
-    if [ "$IsTable" == "Y" ] || [ "$IsTable" == "y" ]
+    if [ "$select" == "1" ] 
+      then
+      ##來源：paper icon 的 github(修改)
+      echo -e "\033[1;32m"
+      echo "正在安裝Google-paper-icon..."
+      echo -e "\033[0m"
+      mkdir .PAPER-ICON
+      cd .PAPER-ICON
+      git clone https://github.com/snwh/paper-icon-theme.git
+      ./autogen.sh
+      make
+      make install
+      cd ..
+      rm -rf .PAPER-ICON
+    elif [ "$select" == "2" ] 
+      then
+      ##來源：numix-icon 的 github（修改）
+      echo -e "\033[1;32m"
+      echo "正在安裝Numix-icon-circle..."
+      echo -e "\033[0m"
+      add-apt-repository ppa：numix / ppa
+      apt update
+      apt install numix-icon-theme-circle
+      apt install 
+    elif [ "$select" == "3" ]
+      then
+      echo -e "\033[1;32m"
+      echo "正在安裝Breeze-icon..."
+      echo -e "\033[0m"
+      mkdir .BREEZE-ICON
+      cd .BREEZE-ICON
+      git clone https://github.com/NitruxSA/breeze-icon-theme.git
+      cp Breeze /usr/share/icons
+      cp Breeze\ Dark /usr/share/icons
+      cd ..
+      rm -rf .BREEZE-ICON
+    elif [ "$select" == "4" ]
       then 
-      ##DoInstall
-      break
-    elif [ "$IsTable" == "N" ] || [ "$IsTable" == "n" ]
-      then break
+        echo "好的，本次不安裝圖標"
+        break
     fi
+
   done
+
 
 ##安裝其他必要程式
 
@@ -167,9 +147,11 @@ echo -e "\033[1;32m"
 echo "正在安裝其他必要程式..."
 echo -e "\033[0m"
 apt install -y vim
+sudo add-apt-repository ppa:djcj/screenfetch
+apt install -y screenfetch
 
 ##Chrome?
-while [ 1 ]
+while :
   do
     echo -e "\033[1;32m"
     printf "你想要安裝Chrome嗎？(Y/N)"
@@ -178,7 +160,7 @@ while [ 1 ]
     if [ "$IsChrome" == "Y" ] || [ "$IsChrome" == "y" ]
       then
       wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
-      sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+      sh -c 'echo "deb [arch=' + $(dpkg --print-architecture) + '] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
       apt update
       apt install google-chrome-stable
       break
@@ -188,7 +170,7 @@ while [ 1 ]
   done
 
 ##工程師專用選項
-while [ 1 ]
+while :
   do
     echo -e "\033[1;32m"
     printf "你是程式工程師嗎？(Y/N)"
@@ -196,7 +178,7 @@ while [ 1 ]
     echo -e "\033[0m"
     if [ "$IsProgramer" == "Y" ] || [ "$IsProgramer" == "y" ]
       then 
-      while [ 1 ]
+      while :
         do
           echo -e "\033[1;32m"
           printf "你需要在此電腦上安裝伺服器程式嗎？(Y/N)"
@@ -209,17 +191,25 @@ while [ 1 ]
                 echo -e "\033[1;32m"
                 echo "請選擇服務器："
                 echo "1) Apach"
-                echo "2) negix"
+                echo "2) Nginx"
+                echo "3) 結束"
                 printf "選擇選項：（請輸入代號）"
                 read DoServerType
                 echo -e "\033[0m"
-                if [ "$DoServer" == "1" ]
-                  then break
-                elif [ "$DoServer" == "2" ]
+                if [ "$DoServerType" == "1" ]
+                  then 
+                    apt install apache2
+                    break
+                elif [ "$DoServerType" == "2" ]
+                  then 
+                    break
+                    apt install nginx
+                elif [ "$DoServerType" == "3" ]
                   then break
                 fi
               done
-            while [ 1 ]
+
+            while :
               do
                 echo -e "\033[1;32m"
                 echo "請選擇資料庫："
@@ -227,17 +217,24 @@ while [ 1 ]
                 echo "2) NoSQL"
                 echo "3) MariDB"
                 echo "4) MongoDB"
+                echo "5) 結束"
                 echo "\"已預裝PHP與phpmyadmin\""
                 printf "選擇選項：（請輸入代號）"
                 read DoDBType
                 echo -e "\033[0m"
                 if [ "$DoDBType" == "1" ]
                   then break
+                  #Install
                 elif [ "$DoDBType" == "2" ]
                   then break
+                  #Install
                 elif [ "$DoDBType" == "3" ]
                   then break
+                  #Install
                 elif [ "$DoDBType" == "4" ]
+                  then break
+                  #Install
+                elif [ "$DoDBType" == "5" ]
                   then break
                 fi
               done
@@ -247,7 +244,7 @@ while [ 1 ]
           fi
         done
         		
-      while [ 1 ]
+      while :
         do
           echo -e "\033[1;32m"
           printf "你需要基於GUI的純文字編輯嗎？(Y/N)"
@@ -255,7 +252,7 @@ while [ 1 ]
           echo -e "\033[0m"
           if [ "$IsGUI" == "Y" ] || [ "$IsGUI" == "y" ]
             then
-            while [ 1 ]
+            while :
               do
                 echo -e "\033[1;32m"
                 echo "請選則編輯器："
@@ -263,7 +260,7 @@ while [ 1 ]
                 echo "2) Sublime3"
                 echo "3) Gvim"
                 echo "4) Visual Studio Code"
-		echo "5) 結束"
+            		echo "5) 結束"
                 printf "選擇選項：（請輸入代號）"
                 read EditSelect
                 echo -e "\033[0m"
@@ -288,47 +285,14 @@ while [ 1 ]
             then break
           fi
         done
-      while [ 1 ]
-        do
-          echo -e "\033[1;32m"
-          printf "你需要Android Studio嗎？(Y/N)"
-          read IsAndroid
-          echo -e "\033[0m"
-          if [ "$ISAndroid" == "Y" ] || [ "$IsAndroid" == "y" ]
-            then 
-            ##Install
-            break
-          elif [ "$ISAndroid" == "N" ] || [ "$IsAndroid" == "n" ]
-            then break
-          fi
-        done			
-      while [ 1 ]
-        do
-          echo -e "\033[1;32m"
-          printf "你需要Unity5.5.0-beta遊戲引擎嗎？(Y/N)"
-          read IsUnity
-          echo -e "\033[0m"
-          if [ "$IsUnity" == "Y" ] || [ "$IsUnity" == "y" ]
-            then
-              mkdir Unity5.5.0-bata
-              cd Unity5.5.0-bata
-              wget http://download.unity3d.com/download_unity/linux/unity-editor-5.5.0b1+20160830_amd64.deb
-              dpkg -i ./unity-editor-5.5.0b1+20160830_amd64.deb
-              cd ..
-              rm -rf Unity5.5.0-bata
-              break
-          elif [ "$IsUnity" == "N" ] || [ "$IsUnity" == "n" ]
-            then break
-          fi
-        done
         break
     elif [ "$IsProgramer" == "N" ] || [ "$IsProgramer" == "n" ]
       then break
     fi
   done
 
-##Wine
-while [ 1 ]
+##安裝Wine
+while :
   do
     echo -e "\033[1;32m"
     printf "需要立即安裝Wine嗎? (Y/N)"
@@ -337,7 +301,7 @@ while [ 1 ]
       if [ "$IsWine" == "Y" ] || [ "$IsWine" == "y" ]
         then 
         apt install -y wine
-        while [ 1 ] 
+        while :
           do 
             echo -e "\033[1;32m"  
             printf "需要順便安裝PlayOnLinux嗎? (Y/N)"
@@ -346,8 +310,7 @@ while [ 1 ]
             if [ "$IsPlayOn" == "Y" ] || [ "$IsPlayOn" == "y" ]
               then
               wget -q "http://deb.playonlinux.com/public.gpg" -O- | sudo apt-key add -
-              wget http://deb.playonlinux.com/playonlinux_trusty.list -O 
-              /etc/apt/sources.list.d/playonlinux.list
+              wget http://deb.playonlinux.com/playonlinux_trusty.list -O /etc/apt/sources.list.d/playonlinux.list
               apt update
               apt install playonlinux
               break
@@ -367,16 +330,20 @@ echo "安裝結束"
 echo -e "\033[0m"
 
 ##詢問重新啟動
-while [ 1 ]
+while :
   do
     echo -e "\033[1;32m"
     printf "需要立即重新啟動嗎？(Y/N)"
     read DoReboot
     echo -e "\033[0m"
     if [ "$DoReboot" == "Y" ] || [ "$DoReboot" == "y" ]
-      then reboot now
+      then
+        screenfetch
+        reboot now
       break
     elif [ "$DoReboot" == "N" ] || [ "$DoReboot" == "n" ]
-      then break
+      then
+        screenfetch
+        break
     fi
   done
